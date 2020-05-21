9f5000780db266252138baf86d390b212bb07b24
Gobral <umpalump97@gmail.com>
2020-05-21 23:35:21 +0200

    dziala zbieranie sklepow

--- app/src/main/java/com/example/myapplication/SprawdzanieNowych.kt
+++ app/src/main/java/com/example/myapplication/SprawdzanieNowych.kt
@@ -4,24 +4,46 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
 import org.jsoup.Jsoup
 import org.jsoup.select.Elements
+import java.io.IOException
+
 
 class SprawdzanieNowych(aktywnosc: AppCompatActivity) : AsyncTask<Void, Void, Int>() {
     private val aktywnoscReference = aktywnosc
+    private val exapi = "http://192.168.1.147:5000/api/okazje"
+    private val client = OkHttpClient();
+    val JSON = "application/json; charset=utf-8".toMediaType()
     override fun doInBackground(vararg params: Void?): Int? {
         println("Start skanowania")
+
        // while (!verifyAvailableNetwork(aktywnoscReference)) {
         //    Thread.sleep(3000)
         //}
-        for(i in 1 until 180) {
+        val doctemp = Jsoup.connect("https://www.euro.com.pl/search/stan-outlet-doskonaly:outlet-dobry,d10,strona-1.bhtml?keyword=outlet&searchType=tag").maxBodySize(0).get().body()
+        val ostatnia = doctemp.getElementsByClass("paging-number").last().text().toInt() + 1
+        for(i in 1 until ostatnia) {
             val lista = Elements()
-            val doc = Jsoup.connect("https://www.euro.com.pl/search/stan-outlet-doskonaly:outlet-dobry,d10,strona-$i.bhtml?keyword=outlet&searchType=tag").get()
+            val doc = Jsoup.connect("https://www.euro.com.pl/search/stan-outlet-doskonaly:outlet-dobry,d10,strona-$i.bhtml?keyword=outlet&searchType=tag").maxBodySize(0).get().body()
             lista.addAll(doc.select("a[class=js-save-keyword]"))
-            println(i)
+            //<h2 class="product-name">
+            //lista.addAll(doc.select("h2[class=product-name]"))
+            //lista.addAll(doc.getElementsByClass("product-name"))
+            println(i.toString() + " " + lista.size);
+            var czy = true
             for (c in lista) {
-               // println(c.text())
-                //println(c.attr("href"))
+                if(czy) {
+                    dodaj(c.text(), "https://www.euro.com.pl" + c.attr("href"))
+                    //println(c.text())
+                    //println(c.attr("href"))
+                }
+                czy = !czy
             }
         }
         return 0
@@ -35,5 +57,20 @@ class SprawdzanieNowych(aktywnosc: AppCompatActivity) : AsyncTask<Void, Void, In
         val connectivityManager=activity.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val networkInfo=connectivityManager.activeNetwork
         return  networkInfo!=null
+    }
+    fun dodaj(nazwa: String, link: String) {
+
+        val body: RequestBody = ("{\"strona\": \"$link\", \"nazwa\": \"$nazwa\" }").toRequestBody(JSON)
+        val request: Request = Request.Builder()
+            .url(exapi)
+            .post(body)
+            .build()
+
+        try {
+            val response: Response = client.newCall(request).execute()
+            response.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
     }
 }
